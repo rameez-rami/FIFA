ceeba9fcdf6e3ffc101ea7aa19b9ac4babcb9d1c
Rameez <rameez11462@gmail.com>
2024-10-30 14:10:06 +0530

    first

---
+++ fifa/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Dummy from './components/Dummy';
+function App() {
+  return (
+    <div>
+      <Dummy/>
+    </div>
+  );
+}
+
+export default App;
